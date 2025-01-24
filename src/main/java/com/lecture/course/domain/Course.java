54138c0f0877e779c60a1eba2f663a5c978dfae4
linirini <2001yerin@naver.com>
2025-01-24 22:15:38 +0900

    feat: 강좌의 수강 신청 수와 신청률 갱신 도메인 로직 구현

--- src/main/java/com/lecture/course/domain/Course.java
+++ src/main/java/com/lecture/course/domain/Course.java
@@ -58,4 +58,13 @@ public class Course extends BaseEntity {
     public Course(String title, long capacity, long price, Member member) {
         this(new Title(title), new Capacity(capacity), new Price(price), member);
     }
+
+    public void enrolled() {
+        enrollCount = enrollCount + 1;
+        enrollRatio = getRatio();
+    }
+
+    private double getRatio() {
+        return Math.round((enrollCount.doubleValue() / capacity.getCapacity()) * 10000) / 100.0;
+    }
 }
--- src/test/java/com/lecture/course/domain/CourseTest.java
+++ src/test/java/com/lecture/course/domain/CourseTest.java
@@ -2,15 +2,19 @@ package com.lecture.course.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import com.lecture.fixture.CourseFixture;
+import com.lecture.fixture.MemberFixture;
 import com.lecture.member.domain.Member;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class CourseTest {
 
     @DisplayName("강좌를 생성할 수 있다.")
     @Test
-    void createMember() {
+    void createCourse() {
         // given
         Member member = new Member("linirini", "linirini@email.com", "01012345678", "password1", "TEACHER");
         String title = "너나위의 내집마련 기초반";
@@ -19,5 +23,21 @@ class CourseTest {
 
         // when
         assertThatNoException().isThrownBy(() -> new Course(title, capacity, price, member));
+    }
+
+    @DisplayName("강좌가 수강 신청이 되었으면 수강 인원수와 수강 신청률을 갱신한다.")
+    @Test
+    void enrolled() {
+        // given
+        Course course = CourseFixture.create(8, MemberFixture.createTeacher());
+
+        // when
+        course.enrolled();
+
+        // then
+        assertAll(
+                () -> assertThat(course.getEnrollCount()).isEqualTo(1),
+                () -> assertThat(course.getEnrollRatio()).isEqualTo(12.5)
+        );
     }
 }
