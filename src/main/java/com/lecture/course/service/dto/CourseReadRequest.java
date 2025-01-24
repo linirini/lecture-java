7f6be76ddcaf3f68a893eeecdb92476d269d2208
linirini <2001yerin@naver.com>
2025-01-24 15:17:55 +0900

    refactor: 쿼리 스트링 구성 수정

--- src/main/java/com/lecture/course/service/dto/CourseReadRequest.java
+++ src/main/java/com/lecture/course/service/dto/CourseReadRequest.java
@@ -1,7 +1,25 @@
 package com.lecture.course.service.dto;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import com.lecture.course.service.OrderColumn;
+
 public record CourseReadRequest(
-        long page,
-        String sort
+        int pageNo,
+        int pageSize,
+        String orderColumn,
+        String orderType
 ) {
+
+    public Pageable toPageable() {
+        return PageRequest.of(mapToPhysicalPageNo(pageNo), pageSize, Direction.fromString(orderType), OrderColumn.findByCondition(orderColumn));
+    }
+
+    private int mapToPhysicalPageNo(int pageNo) {
+        if (pageNo <= 0) {
+            return 0;
+        }
+        return pageNo - 1;
+    }
 }
