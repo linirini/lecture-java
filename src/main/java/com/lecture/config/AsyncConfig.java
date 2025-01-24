2ee106faebefa8655103bf2869bf3803160b8f88
linirini <2001yerin@naver.com>
2025-01-24 22:27:07 +0900

    feat: 비동기 설정 파일 추가

---
+++ src/main/java/com/lecture/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.lecture.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
