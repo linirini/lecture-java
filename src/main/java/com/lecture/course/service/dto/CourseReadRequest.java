1cf23bc154130f362eefa325e39a4c4c8f90d569
linirini <2001yerin@naver.com>
2025-01-24 17:12:44 +0900

    feat: 값이 없거나, 일치하지 않을 때 기본 값을 사용하도록 구현

--- src/main/java/com/lecture/course/service/dto/CourseReadRequest.java
+++ src/main/java/com/lecture/course/service/dto/CourseReadRequest.java
@@ -1,25 +1,59 @@
 package com.lecture.course.service.dto;
 
+import java.util.Objects;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import com.lecture.course.service.OrderColumn;
 
 public record CourseReadRequest(
-        int pageNo,
-        int pageSize,
+        Integer pageNo,
+        Integer pageSize,
         String orderColumn,
         String orderType
 ) {
 
+    private static final int DEFAULT_PAGE_NO = 0;
+    private static final int DEFAULT_PAGE_SIZE = 20;
+    private static final Direction DEFAULT_DIRECTION = Direction.DESC;
+    private static final OrderColumn DEFAULT_ORDER_COLUMN = OrderColumn.RECENT;
+
     public Pageable toPageable() {
-        return PageRequest.of(mapToPhysicalPageNo(pageNo), pageSize, Direction.fromString(orderType), OrderColumn.findByCondition(orderColumn));
+        return PageRequest.of(
+                getPageNo(pageNo),
+                getPageSize(pageSize),
+                getDirection(orderType),
+                getOrderColumn(orderColumn)
+        );
     }
 
-    private int mapToPhysicalPageNo(int pageNo) {
-        if (pageNo <= 0) {
-            return 0;
+    private int getPageSize(Integer pageSize) {
+        if (Objects.isNull(pageSize) || pageSize <= 0) {
+            return DEFAULT_PAGE_SIZE;
+        }
+        return pageSize;
+    }
+
+    private int getPageNo(Integer pageNo) {
+        if (Objects.isNull(pageNo) || pageNo <= 0) {
+            return DEFAULT_PAGE_NO;
         }
         return pageNo - 1;
+    }
+
+    private Direction getDirection(String orderType) {
+        try {
+            return Direction.fromString(orderType);
+        } catch (IllegalArgumentException e) {
+            return DEFAULT_DIRECTION;
+        }
+    }
+
+    private String getOrderColumn(String orderColumn) {
+        try {
+            return OrderColumn.findByCondition(orderColumn).getColumn();
+        } catch (IllegalArgumentException e) {
+            return DEFAULT_ORDER_COLUMN.getColumn();
+        }
     }
 }
