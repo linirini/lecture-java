32ea4dd9812db373983429698e8f148f0a758aee
linirini <2001yerin@naver.com>
2025-01-31 18:40:30 +0900

    feat: 수강 신청 시 중복 요청 제거

--- src/main/java/com/lecture/enrollment/service/dto/EnrollmentRequest.java
+++ src/main/java/com/lecture/enrollment/service/dto/EnrollmentRequest.java
@@ -6,8 +6,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "수강 신청 요청 형식입니다.")
 public record EnrollmentRequest(
-        @Schema(example = "[1,2,3]")
+        @Schema(description = "중복된 강의 ID는 한 번만 수강 신청 요청이 발생합니다.", example = "[1,2,3]")
         @NotNull(message = "강좌 정보를 입력해주세요.")
         List<Long> courseIds
 ) {
+
+    @Override
+    public List<Long> courseIds() {
+        return courseIds.stream()
+                .distinct()
+                .toList();
+    }
 }
